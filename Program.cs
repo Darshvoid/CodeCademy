512d2bdaa4ca5c25c75c76c94acb7a6523b12d55
Ernesto delgado <Darshvoid@gmail.com>
2023-02-08 16:53:44 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,167 @@
+﻿using System;
+
+namespace ExquisiteCorpse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Type RANDOM for randomly built creature or CUSTOM to build the creature: ");
+            string choice = Console.ReadLine();
+            choice = choice.ToUpper();
+            if (choice == "RANDOM")
+            {
+                BuildACreature();
+            }
+            else if(choice == "CUSTOM")
+            {
+                Console.Write("For ghost head type \"ghost\", for bug head type \"bug\", for monster head type \"monster\" : ");
+                string head = Console.ReadLine();
+                head = head.ToLower();
+                Console.Write("For ghost body type \"ghost\", for bug body type \"bug\", for monster body type \"monster\" : ");
+                string body = Console.ReadLine();
+                body = body.ToLower();
+                Console.Write("For ghost feet type \"ghost\", for bug feet type \"bug\", for monster feet type \"monster\" : ");
+                string feet = Console.ReadLine();
+                feet = feet.ToLower();
+                BuildACreature(head, body, feet);
+            }
+
+        }
+        static void BuildACreature(string head, string body, string feet)
+        {
+            int headNum = TranslateToNumber(head);
+            int bodyNum = TranslateToNumber(body);
+            int feetNum = TranslateToNumber(feet);
+            SwitchCase(headNum, bodyNum, feetNum);
+        }
+
+        static void BuildACreature()
+        {
+            RandomMode();
+        }
+        static void RandomMode()
+        {
+            Random randomNumber = new Random();
+            int head = randomNumber.Next(1, 4);
+            int body = randomNumber.Next(1, 4);
+            int feet = randomNumber.Next(1, 4);
+            SwitchCase(head, body, feet);
+        }
+        static void SwitchCase(int head, int body, int feet)
+        {
+            switch (head)
+            {
+                case 1:
+                    MonsterHead();
+                    break;
+                case 2:
+                    GhostHead();
+                    break;
+                case 3:
+                    BugHead();
+                    break;
+            }
+            switch (body)
+            {
+                case 1:
+                    MonsterBody();
+                    break;
+                case 2:
+                    GhostBody();
+                    break;
+                case 3:
+                    BugBody();
+                    break;
+            }
+            switch (feet)
+            {
+                case 1:
+                    MonsterFeet();
+                    break;
+                case 2:
+                    GhostFeet();
+                    break;
+                case 3:
+                    BugFeet();
+                    break;
+            }
+        }
+        static int TranslateToNumber(string creature)
+        {
+            switch (creature)
+            {
+                case "monster":
+                    return 1;
+                case "ghost":
+                    return 2;
+                case "bug":
+                    return 3;
+                default:
+                    return 1;
+            }
+        }
+        static void GhostHead()
+        {
+            Console.WriteLine("     ..-..");
+            Console.WriteLine("    ( o o )");
+            Console.WriteLine("    |  O  |");
+        }
+
+        static void GhostBody()
+        {
+            Console.WriteLine("    |     |");
+            Console.WriteLine("    |     |");
+            Console.WriteLine("    |     |");
+        }
+
+        static void GhostFeet()
+        {
+            Console.WriteLine("    |     |");
+            Console.WriteLine("    |     |");
+            Console.WriteLine("    '~~~~~'");
+        }
+
+        static void BugHead()
+        {
+            Console.WriteLine("     /   \\");
+            Console.WriteLine("     \\. ./");
+            Console.WriteLine("    (o + o)");
+        }
+
+        static void BugBody()
+        {
+            Console.WriteLine("  --|  |  |--");
+            Console.WriteLine("  --|  |  |--");
+            Console.WriteLine("  --|  |  |--");
+        }
+
+        static void BugFeet()
+        {
+            Console.WriteLine("     v   v");
+            Console.WriteLine("     *****");
+        }
+
+        static void MonsterHead()
+        {
+            Console.WriteLine("     _____");
+            Console.WriteLine(" .-,;='';_),-.");
+            Console.WriteLine("  \\_\\(),()/_/");
+            Console.WriteLine("　  (,___,)");
+        }
+
+        static void MonsterBody()
+        {
+            Console.WriteLine("   ,-/`~`\\-,___");
+            Console.WriteLine("  / /).:.('--._)");
+            Console.WriteLine(" {_[ (_,_)");
+        }
+
+        static void MonsterFeet()
+        {
+            Console.WriteLine("    |  Y  |");
+            Console.WriteLine("   /   |   \\");
+            Console.WriteLine("   \"\"\"\" \"\"\"\"");
+        }
+    }
+}
